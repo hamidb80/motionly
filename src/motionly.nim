3e5b7220a223f13b6975432c646a98f4abaea7b5
hamidb80 <hr.bolouri@gmail.com>
2021-11-29 01:45:44 +0330

    add expected animation script

--- src/motionly.nim
+++ src/motionly.nim
@@ -1,7 +1,36 @@
-# This is just an example to get you started. A typical library package
-# exports the main API in this file. Note that you cannot rename this file
-# but you can remove it if you wish.
+when false:
 
-proc add*(x, y: int): int =
-  ## Adds two files together.
-  return x + y
+  expectation:
+    ## on | off with true | false
+    ## no runtime checks
+    ## compile time loop and macro expand
+    ## strong compile checks
+
+    import std / [Table, Array]
+
+    shape tb1 Table {type= int, rows = 4, cols = 3 }
+    shape arr Array {type= int, len= 8, &with_index }
+
+    def ops {}
+    def delay 120ms
+    def degree 54deg
+    def number 66
+
+    ## everyThing that comes in animat
+    call Array.setPosition, arr, 130, 40
+
+    animate {delay = 200ms, executionPolicy = &parallel}:
+      call Array.setIndex, arr, 0, 4
+      call Array.setIndexVisibility, arr, off
+
+
+    call delay 120ms
+
+    macro set0 {num}:
+      call Array.setIndex, arr, 0, num
+
+    
+    loop i =  1..5|2:
+      call set0, i
+
+
