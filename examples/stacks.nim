4c50797151a446d7174584777971b2eddff6b62a
hamidb80 <hr.bolouri@gmail.com>
2022-01-22 11:58:16 +0330

    improve timing of stacks example

--- examples/stacks.nim
+++ examples/stacks.nim
@@ -47,13 +47,13 @@ defTimeline timeline, mystage:
     register @layers[i].tmove(p(0, -50)) ~> (dt, eOutCubic, delay)
 
   flow show(i: int, dt: float):
-    register @layers[i].fadeIn() ~> (dt, eOutQuad)
+    register @layers[i].fadeIn() ~> (dt, eInQuad)
 
   flow hide(i: int, dt: float):
     register @layers[i].fadeOut() ~> (dt, eInOutQuad)
 
   flow showFull(i: int, dt: float):
-    register (StackLayer @layers[i]).full.fadeIn() ~> (dt, eInOutQuad)
+    register (StackLayer @layers[i]).full.fadeIn() ~> (dt, eOutQuad)
 
   before:
     for l in @layers[3 .. ^1]:
@@ -63,30 +63,32 @@ defTimeline timeline, mystage:
       (StackLayer l).full.opacity = 0.0
 
   # --- part 1
-  on 100.ms .. 500.ms:
+  on 0.ms .. 400.ms:
     !hide(0, dt)
     !moveUp(0, dt, 0)
 
-  on 400.ms .. 800.ms:
+  on 300.ms .. 700.ms:
     for i in 1 .. 4:
       !moveUp(i, dt, toFloat i * 100)
 
-  on 600.ms .. 900.ms:
+  on 500.ms .. 800.ms:
     !showFull(1, dt)
     !show(3, dt)
 
   # --- part 2
-  on 1300.ms .. 1700.ms:
+  on 1400.ms .. 1800.ms:
     !hide(1, dt)
     !moveUp(1, dt, 0)
 
-  on 1700.ms .. 2100.ms:
+  on 1800.ms .. 2200.ms:
     for i in 2 .. 4:
       !moveUp(i, dt, toFloat i * 100)
 
-  on 1800.ms .. 2200.ms:
+  on 2000.ms .. 2300.ms:
     !showFull(2, dt)
     !show(4, dt)
 
+  at 2900.ms:
+    discard
 
 timeline.saveGif("./temp/out.gif", mystage, 50.fps, justFirstFrame = false)
