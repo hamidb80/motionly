a409f4ce7dfc22fd81ed0133168a8e1e2193ced1
hamidb80 <hr.bolouri@gmail.com>
2022-01-22 14:05:57 +0330

    update eg2

--- examples/eg2.nim
+++ examples/eg2.nim
@@ -10,6 +10,8 @@ defTimeline timeline, mystage:
   on 400.ms .. 800.ms:
     register @box.tmove(p(100, 100) .. p(0, 100)) ~> (dt, eOutCubic)
 
+  after 300.ms: discard # waste some time
+
   frame 500.ms:
     register @box.tmove(p(100, 0)) ~> (dt, eOutCubic)
 
